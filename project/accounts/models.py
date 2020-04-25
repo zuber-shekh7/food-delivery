75e647650dac23ff3629d4c3836fcaff66bd738f
zuber shekh <zube.shekh@rapidops.com>
2020-04-25 18:08:22 +0530

    added view and list page to restaurant

--- project/accounts/models.py
+++ project/accounts/models.py
@@ -7,6 +7,7 @@ def load_user(id):
     return User.query.get(int(id))
 
 class User(db.Model,UserMixin):
+
     id = db.Column(db.Integer,primary_key=True)
     email = db.Column(db.String(),nullable=False)
     password = db.Column(db.String(),nullable=False)
--- project/restaurants/views.py
+++ project/restaurants/views.py
@@ -2,11 +2,19 @@ from flask import Blueprint,render_template,url_for,redirect,flash,request
 from flask_login import login_user,logout_user,current_user
 
 from project.app import db
-
+from .models import Restaurant
 
 restaurants = Blueprint('restaurants',__name__,url_prefix='/restaurants/',template_folder='templates/')
 
 
 @restaurants.route('/')
 def index():
-    return 'restaurants'
+    restaurants = Restaurant.query.all()
+
+    return render_template('restaurants/list.html',restaurants=restaurants)
+
+@restaurants.route('/restaurant/<int:id>')
+def view(id):
+    restaurant = Restaurant.query.get_or_404(id)
+
+    return render_template('restaurants/view.html',restaurant=restaurant)
